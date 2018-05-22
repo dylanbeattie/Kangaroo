e2698732fd047ae47902ad7fc139ef820e15a659
dylanbeattie <dylan@dylanbeattie.net>
2018-05-22 14:38:57 +0100

    Working prototype without using Mbrewerton.DotNetPivotalTrackerApi

---
+++ Kangaroo.Website/Controllers/PivotalController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Pivotal;
+
+namespace Kangaroo.Website.Controllers {
+    public class PivotalController : Controller {
+        private readonly Tracker tracker;
+
+        public PivotalController(Tracker tracker) {
+            this.tracker = tracker;
+        }
+
+        public ActionResult Me() {
+            return Json(tracker.Me());
+        }
+
+        public ActionResult Projects() {
+            return View(tracker.Projects());
+        }
+
+        public ActionResult Stories(int id) {
+            return View(tracker.Stories(id));
+        }
+    }
+}
---
+++ Pivotal/Tracker.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks.Dataflow;
+using Newtonsoft.Json;
+
+namespace Pivotal {
+    public class Tracker {
+        private const string TRACKER_API_TOKEN = "b1be30c19645d929605c3ffdb23dfad8";
+        private const string BASE_URL = "https://www.pivotaltracker.com/services/v5";
+
+        public dynamic Me() {
+            return Get("/me");
+        }
+
+        private dynamic Get(string path) {
+            using (var wc = new WebClient()) {
+                wc.Headers.Add("X-TrackerToken", TRACKER_API_TOKEN);
+                return wc.DownloadString(BASE_URL + path);
+            }
+        }
+
+        public IList<Project> Projects() {
+            var json = Get("/projects");
+            return JsonConvert.DeserializeObject<List<Project>>(json);
+        }
+
+        public IList<Story> Stories(int projectId) {
+            using (var wc = new WebClient()) {
+                wc.Headers.Add("X-TrackerToken", TRACKER_API_TOKEN);
+                var json = wc.DownloadString($"{BASE_URL}/projects/{projectId}/stories/");
+                // TODO: pagination stuff here?
+                return JsonConvert.DeserializeObject<List<Story>>(json);
+            }
+        }
+    }
+
+    public class Project {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Story {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
